f7922bccff13fd732eb4017d7215f6d839024ea9
lorecn1976 <lorecn@yahoo.es>
2019-04-22 10:57:21 +0200

    cambios hoy

---
+++ JS/02_variables.js
@@ -0,0 +1,46 @@
+//En ES6 se utulizan let y const para declarar variables
+
+let nombre //declarar
+nombre='Pepe'//inicializar
+
+let edad = 34 //Declarar e inicializar
+
+//Tipos de datos
+
+//undefined
+let data
+console.log(data)
+console.log(typeof data)
+
+
+//strings
+data = 'Hola amigos'
+console.log(data)
+console.log(typeof data)
+
+//numbers
+data = 35
+console.log(data)
+console.log(typeof data)
+
+//booleans
+data = true
+data = false
+console.log(data)
+console.log(typeof data)
+
+//object
+data = {nombre: 'Pepe', edad: 34}
+console.log(data)
+console.log(typeof data)
+
+//object tipo array
+data = [23, 45, 78]
+console.log(data)
+console.log(typeof data)
+
+
+function mostrar(){}
+
+console.log(mostrar)
+console.log(typeof mostrar)
