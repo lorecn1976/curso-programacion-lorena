7c591fae5ce418aede354490c5796cf4dfe05cfd
lorecn76 <lorecn@yahoo.es>
2019-04-28 22:31:20 +0200

    cambios hoy

---
+++ JS/16_fibonacci.js
@@ -0,0 +1,26 @@
+/**
+ * Presentar la serie Fibonacci
+ * 
+ * la regla es xn = xn-1 + xn-2
+ */
+/**
+ * 
+ * function Fibonacci
+ * @param num:number
+ * @returns:array[number]
+ */
+
+number=3000
+
+function Fibonacci(num) {
+    let Fibonacci = []
+    for (let i = 0; i <= limite; i++)}
+    
+    {
+        ;
+        return xn
+    }
+
+
+
+    return x
