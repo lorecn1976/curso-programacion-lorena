29fad798b0c6cc1ed53728f66ab7648d3e991c22
lorecn1976 <lorecn@yahoo.es>
2019-05-06 12:31:42 +0200

    cambios hoy

---
+++ JS/24_fechas.js
@@ -0,0 +1,14 @@
+let meses = {
+    airline: 'Oceanic',
+    number: '815',
+    departure: {
+        claveIATA: 'SYD',
+        time:  '2004-09-22 14:55',
+        ciudad: 'Sydney'
+    },
+    arrival: {
+        claveIATA: 'LAX',
+        time:  '2004-09-23 10:42',
+        ciudad: 'Los Angeles'
+    }
+}
