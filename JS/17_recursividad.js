f95eb66ad5cabe6d9cb89b308e14ea0b6709321c
lorecn1976 <lorecn@yahoo.es>
2019-05-06 09:16:36 +0200

    cambios hoy

--- JS/17_recursividad.js
+++ JS/17_recursividad.js
@@ -1,32 +1,40 @@
 /**
- * Calculo del factorial mediante recursividad 
- * 
+ * Calculo del factorial mediante recursividad
  */
 
-/* Function factorial
-* @param num: number
-* @returns: num
-* Se calcula el producto de num por todos los inferiores a él hasta el 1
-* ees decir, el producto de 1 por todos los superiores hasta llegar a num
-*   
-*/
+  /**
+  * Function factorial
+  * @param num: number
+  * @returns: num
+  * 
+  * Se calcula el producto de num por todos los inferiores a él hasta el 1
+  * es decir el producto de 1 por todos los superiores a él hasta llegar a num
+  * 
+  */
 
-
-
-function factorial(num) {
-       if (num <= 0) {
-        return 0//entraria el 0 y los negativos
+ function factorial(num) {
+    if (num < 0 || parseInt(num) !== num)  {
+        throw 'Los números negativos o decimales no tienen factorial' 
+    } else if  ( num === 0) {
+        return 1
+    } else if ( num === 1) {
+        return num
+    } else if (num > 170 ) {
+        throw 'No puedo calcular factoriales de números tan grandes'
     }
-    if (num === 1) {  
-        return num   
-    }
-    return num* factorial(num-1)
+    return num * factorial(num-1)
 }
 
-
-let n=5
-n=0
-n=64
-console.log(factorial(64))
-
-//copiar su ejercicio 17
+let n = 0
+n = -3
+n = 4.6
+n = 1
+n = 5
+n = 6
+n = 170
+n = 171
+try {
+     console.log(factorial(n))
+} catch (error) {
+    console.log(error)
+}
